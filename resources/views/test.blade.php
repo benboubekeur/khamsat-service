341e67f59d8b54fa4b01301c706eec3737a9e083
Boumedyen_Aventique <boumedyen.aventique@medicalib.fr>
2022-08-12 20:58:23 +0100

    auth

---
+++ resources/views/test.blade.php
@@ -0,0 +1,20 @@
+
+@extends('adminlte::page')
+
+@section('title', 'Test')
+
+@section('content_header')
+    <h1>Test</h1>
+@stop
+
+@section('content')
+    <p>Test</p>
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
